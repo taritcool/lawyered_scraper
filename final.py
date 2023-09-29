e411322d17187c29a91335329ae305f2d34d153d
Tarit Kumar Rez <36450398+taritcool@users.noreply.github.com>
2023-09-29 20:25:56 +0530

    final draft

---
+++ final.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import re
+from bs4 import BeautifulSoup
+
+url = 'https://www.lawyered.in/legal-disrupt?categoryId='
+
+result = {}
+for i in range(6):
+    if i != 2:
+        res = requests.get(url + str(i+1))
+        soup = BeautifulSoup(res.content, 'html.parser')
+        articles = soup.find_all('div', attrs={'class':'small-post mb-5'})
+        contents = {}
+        for k, article in enumerate(articles):
+            content = {}
+            content['article_link'] = 'https://www.lawyered.in/' + article.h5.a.get('href')
+            content['article_title'] = article.h5.text.strip()
+            content['article_desc'] = article.p.text.strip()
+            content['article_published_date'] = article.find_all('li')[0].text.strip()
+            content['time_to_read'] = article.find_all('li')[1].text.strip()
+            contents[k] = content
+        title = soup.find_all('a', attrs={'href':'/legal-disrupt?categoryId=' + str(i+1)})[0].text.strip()
+        result[title] = contents
+
+with open("final.json", "w") as outfile:
+    json.dump(result, outfile)
